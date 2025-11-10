ec85abb777cf3aa6750c68b94e29c1b49d9951d6
vassu <vassuthopuri7@gmail.com>
2025-11-10 13:09:45 -0500

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,11 @@
+import Greeting from "./components/Task";
+
+export default function Title() {
+  return (
+    <div>
+      <h1> Hello </h1>
+      <p> This is my first react application</p>
+      <Greeting />
+    </div>
+  );
+}
---
+++ src/components/Task.jsx
@@ -0,0 +1,7 @@
+export default function Greeting() {
+  return (
+    <div>
+      <p> Have a good one</p>
+    </div>
+  );
+}
